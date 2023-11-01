f943484c48c22cb526bf06eb89d7c357e75bef6c
Davi Andrade <149514841+swttzz@users.noreply.github.com>
2023-10-31 23:39:21 -0300

    1 Update
    +3 ACS Scripts
    +MISC

---
+++ swtzhub.lua
@@ -0,0 +1,96 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "SWTTZZ HUB", HidePremium = true, IntroText = "SWTTZZ", SaveConfig = true, ConfigFolder = "OrionTest"})
+
+--ANIME CHAMPIONS SCRIPTS
+
+local Tab1 = Window:MakeTab({
+	Name = "ACS Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab1:AddSection({
+	Name = "Best Hubs"
+})
+
+Tab1:AddButton({
+	Name = "Magenta HUB",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Odrexyo/Script/main/Loader.lua"))()
+      		
+  	end    
+})
+
+Tab1:AddButton({
+	Name = "Banana HUB",
+	Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/diepedyt/bui/main/temporynewkeysystem.lua"))()
+      		
+  	end    
+})
+
+Tab1:AddButton({
+	Name = "Sky HUB",
+	Callback = function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/SKOIXLL/RIVERHUB-SKYHUB/main/WL.lua'))();
+      		
+  	end    
+})
+
+--[[
+ESPAÇO PARA JOGO
+
+local Tab4 = Window:MakeTab({
+	Name = "Sem nome",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+]]
+
+--[[
+ESPAÇO PARA JOGO
+
+local Tab4 = Window:MakeTab({
+	Name = "Sem nome",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+]]
+
+--[[
+ESPAÇO PARA JOGO
+
+local Tab4 = Window:MakeTab({
+	Name = "Sem nome",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+]]
+
+--MISC
+
+local Tab5 = Window:MakeTab({
+	Name = "Misc",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local Section = Tab5:AddSection({
+	Name = "Miscellaneous"
+})
+
+Tab5:AddButton({
+	Name = "Turn OFF GPU",
+	Callback = function()
+        game:GetService("RunService"):Set3dRenderingEnabled(false)
+      		
+  	end    
+})
+
+Tab5:AddButton({
+	Name = "Turn ON GPU",
+	Callback = function()
+        game:GetService("RunService"):Set3dRenderingEnabled(true)
+      		
+  	end    
+})
